f2664cf44da3586d8a5f0f4d9c5ec85077a4f3e0
rafdianr <rafdian.ramadhan@gmail.com>
2020-06-30 09:16:36 +0700

    [Rafd] - add route services page

---
+++ src/pages/ServicePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ServicePage = () => {
+  return (
+    <div>
+      <h1>This is from ServicePage.js</h1>
+    </div>
+  );
+};
+
+export default ServicePage;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,22 +1,23 @@
-import React, {Fragment} from "react";
-import {BrowserRouter as Router, Switch,Route} from "react-router-dom";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
-import Header from '../pages/layouts/Header'
-import Footer from '../pages/layouts/Footer'
-
+import Header from "../pages/layouts/Header";
+import Footer from "../pages/layouts/Footer";
+import ServicePage from "../pages/ServicePage";
 
 const Routes = () => {
   return (
     <Fragment>
       <Router>
-        <Header/>
+        <Header />
         <Switch>
           <Route path="/" component={HomePage} exact />
+          <Route path="/service" component={ServicePage} exact />
         </Switch>
-        <Footer/>
+        <Footer />
       </Router>
     </Fragment>
-  )
-}
+  );
+};
 
 export default Routes;
